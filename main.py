64ad63b33a07bdfb08a82d5687893cbff9a6a612
felipereyel <felipe.reyel@gmail.com>
2021-11-25 09:12:28 -0300

    limit votes

--- main.py
+++ main.py
@@ -38,10 +38,13 @@ async def on_message(message):
 
         user_rh_state = rh_state.get(user_id, [])
         if len(user_rh_state) < NUM_VOTES - 1:
-            rh_state[user_id] = [*user_rh_state, message.author.id]
-            await message.channel.send(
-                f"{reference} {len(rh_state[user_id])}/{NUM_VOTES}"
-            )
+            if message.author.id in user_rh_state:
+                await message.channel.send("Voce ja votou")
+            else:
+                rh_state[user_id] = [*user_rh_state, message.author.id]
+                await message.channel.send(
+                    f"{reference} {len(rh_state[user_id])}/{NUM_VOTES}"
+                )
         else:
             for member in client.get_all_members():
                 if member.id == user_id:
