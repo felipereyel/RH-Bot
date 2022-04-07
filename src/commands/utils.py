73f8502aa7032784153ea1fb5906de82e60c40af
Felipe Reyel <felipe.reyel@gmail.com>
2022-04-07 11:42:06 -0300

    fix regex

--- src/commands/utils.py
+++ src/commands/utils.py
@@ -3,6 +3,6 @@ import re
 
 def search_user(CALL, message):
     reference = message.replace(CALL, "").strip()
-    search = re.search(r"<@!(\d*)>", reference)
+    search = re.search(r"(\d*)", reference)
 
     return [reference, search]
